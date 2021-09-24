f0f9129f180d2556a0920741538d4481f7014761
juancanas45 <84482151+juancanas45@users.noreply.github.com>
2021-09-24 16:42:39 -0500

    Create validaciones.js

---
+++ validaciones.js
@@ -0,0 +1,15 @@
+function validar_nombre_usuario(string){
+
+} 
+
+function validar_contrasena(string){
+
+} 
+
+function confirmar_contrasena(stringA, stringB){
+
+} 
+
+module.exports.validar_nombre_usuario=validar_nombre_usuario;
+module.exports.validar_contrasena=validar_contrasena;
+module.exports.confirmar_contrasena=confirmar_contrasena;
