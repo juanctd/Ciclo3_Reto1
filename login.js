20f9cf9c4cb438a487b5748ad5286dbdc2598f7d
juancanas45 <juan_carlos_canas@hotmail.com>
2021-10-15 19:56:21 -0500

    Update login.js
    .

--- login.js
+++ login.js
@@ -2,7 +2,12 @@ let registros = [];
 
 function validar_captcha(rcaptcha) 
 {
-  
+    if ( rcaptcha=="5") {
+        return true;
+
+                }else {
+            return false;
+        }
   
   
 }
@@ -12,6 +17,9 @@ function iniciar_session(usuario,contrasena,rchaptcha) {
     var usuario=document.getElementById('login_username').value;
   
     var contrasena=document.getElementById('login_password').value;
+
+    var rchaptcha=document.getElementById('login_captcha').value;
+
     registros.push({"usuario":usuario, "contrasena":contrasena, "rchaptcha":rchaptcha});
 
 }
