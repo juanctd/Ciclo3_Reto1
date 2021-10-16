b131653035ae76089c47c041bf35ddff4f9be408
juancanas45 <juan_carlos_canas@hotmail.com>
2021-10-16 11:09:37 -0500

    Update login.js
    100

--- login.js
+++ login.js
@@ -1,37 +1,32 @@
 let registros = [];
 
-function validar_captcha(rcaptcha) 
-{
- var ca=rcaptcha;
- 
-    if (ca == 5) {
-        return true;
-                }else {
-            return false;
-        }
+function validar_captcha(rcaptcha) {
+  var captcha = Number(rcaptcha);
+  if(captcha==="") {
+      return false;
+  } 
+  else if(captcha===5){
+      return true;
   }
+  else
+      return false;
+}
 
-function iniciar_sesion(usuario,contrasena,rcaptcha) {
-    
-    registros.push({"usuario":usuario, "contrasena":contrasena, "rcaptcha":rcaptcha});
-    const r=validar_captcha(registros[0].rcaptcha); 
-    const u=registros[0].usuario;
-    const c=registros[0].contrasena;
-
-    if (r == false) {
-        return false;
-     } else { 
-        return true; }
-           
-    if(u.length == 0 ){
-        return false;
-      } else {  if (r == true) {
-        return true; }    
-
-    if(c.length == 0 ){
-          return false;
-        }
-    
+function iniciar_sesion(usuario, contrasena, rcaptcha) {
+  if(validar_captcha(rcaptcha)){
+      for (const validar in registros) { 
+        if(registros[validar].usuario===usuario && registros[validar].contrasena===contrasena){
+              return true;
+        } 
+        else {
+           continue;
+          }
+      }
+      return false;
+  } 
+  else {
+      return false;
+  }
 }
 
 module.exports.registros = registros;
